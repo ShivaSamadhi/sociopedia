3325238cbab2ff9a44151af56c40a7a80923447e
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-22 14:01:50 -0500

    Navbar component

--- mern_socialapp/client/src/scenes/loginPage/loginPage.jsx
+++ mern_socialapp/client/src/scenes/loginPage/loginPage.jsx
@@ -1,10 +1,41 @@
-import {Box} from "@mui/material";
-import Navbar from "../navbar/navbar";
+import {
+    Box,
+    Typography,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+
 
 const LoginPage = () => {
+    const theme = useTheme()
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
+
     return (
     <Box>
-        <Navbar/>
+        <Box
+            width="100%"
+            backgroundColor={theme.palette.background.alt}
+            p="1rem 6%"
+            textAlign="center"
+        >
+            <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >
+                Sociopedia
+            </Typography>
+        </Box>
+
+        <Box
+            width={isNonMobileScreen ? "50%" : "93%"}
+            p="2rem"
+            m="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor={theme.palette.background.alt}
+        >
+
+        </Box>
     </Box>
     )
 }
