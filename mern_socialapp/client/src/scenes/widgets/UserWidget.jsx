a1481b6e7511a73284f9413bfc395c2af5602276
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-06 14:00:32 -0500

    User Widget

--- mern_socialapp/client/src/scenes/widgets/UserWidget.jsx
+++ mern_socialapp/client/src/scenes/widgets/UserWidget.jsx
@@ -55,6 +55,113 @@ const UserWidget = ({userId, picturePath}) => {
     } = user
 
     return (
+        <WidgetWrapper>
+            {/*First Row*/}
+            <FlexBetween
+                gap=".5rem"
+                pb="1.1rem"
+                onClick={() => navigate(`/profile/${userId}`)}
+            >
+                <FlexBetween gap="1rem">
+                    <UserImage image={picturePath}/>
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            color={dark}
+                            fontWeight="500"
+                            sx={{
+                                "&:hover": {
+                                    color: palette.primary.light,
+                                    cursor: "pointer"
+                                }
+                            }}>
+                            {firstName} {lastName}
+                        </Typography>
+                        <Typography color={medium}>
+                            {friends.length} friends
+                        </Typography>
+                    </Box>
+                </FlexBetween>
+                <ManageAccountsOutlined/>
+            </FlexBetween>
+            <Divider/>
 
+            {/*Second Row*/}
+            <Box
+                p="1rem 0"
+            >
+               <Box
+                   display="flex"
+                   alignItems="center"
+                   gap="1rem"
+                   mb=".5rem"
+               >
+                   <LocationOnOutlined
+                       fontSize='large'
+                       sx={{
+                           color: main
+                       }}
+                   />
+                   <Typography
+                    color={medium}>
+                       {location}
+                   </Typography>
+               </Box>
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    gap="1rem"
+
+                >
+                    <WorkOutlineOutlined
+                        fontSize='large'
+                        sx={{
+                            color: main
+                        }}
+                    />
+                    <Typography
+                        color={medium}>
+                        {occupation}
+                    </Typography>
+                </Box>
+            </Box>
+
+            {/*Third Row*/}
+            <Box p="1rem 0">
+                <FlexBetween mb=".5rem">
+                    <Typography
+                        color={medium}
+                    >
+                       Profile Views
+                    </Typography>
+                    <Typography
+                        color={main}
+                        fontWeight="500"
+                    >
+                        {viewedProfile}
+                    </Typography>
+                </FlexBetween>
+                <FlexBetween mb=".5rem">
+                    <Typography
+                        color={medium}
+                    >
+                        Post Impressions
+                    </Typography>
+                    <Typography
+                        color={main}
+                        fontWeight="500"
+                    >
+                        {impressions}
+                    </Typography>
+                </FlexBetween>
+            </Box>
+
+            {/*Fourth Row*/}
+            <Box p="1rem 0">
+                <Typography
+                    fontSize="1rem"
+                    color={main}
+            </Box>
+        </WidgetWrapper>
     )
 }
