dece319913ffb90728885870057edba6ce68441f
Ramaj Johnson <rjohnson3795@gmail.com>
2023-06-07 09:58:57 -0500

    User Widget

--- mern_socialapp/client/src/scenes/homePage/homePage.jsx
+++ mern_socialapp/client/src/scenes/homePage/homePage.jsx
@@ -1,9 +1,52 @@
 import Navbar from "../navbar/navbar";
+import {Box, useMediaQuery} from "@mui/material";
+import {useSelector} from "react-redux";
+import UserWidget from "../widgets/UserWidget";
 const HomePage = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width:1000px)")
+  const {_id, picturePath} = useSelector((state) => state.user)
+
+
   return (
-      <div>
+      <Box>
           <Navbar/>
-      </div>
+
+          {/*Main Body*/}
+          <Box
+            width="100%"
+            padding="2rem 6%"
+            display={isNonMobileScreen ? "flex" : "block"}
+            gap="0.5rem"
+            justifyContent="space-between"
+          >
+
+            {/*User Widget*/}
+            <Box
+               flexBasis={isNonMobileScreen ? "26%" : undefined}
+            >
+                <UserWidget userId={_id} picturePath={picturePath}/>
+            </Box>
+            {/*User Widget*/}
+
+            {/*Posts Widget*/}
+            <Box
+               flexBasis={isNonMobileScreen ? "42%" : undefined}
+               mt={isNonMobileScreen ? undefined : "2rem"}
+            >
+
+            </Box>
+            {/*Posts Widget*/}
+
+            {/*Friends Widget */}
+              {isNonMobileScreen &&
+                  <Box flexBasis="26%">
+
+                  </Box>}
+            {/*Friends Widget */}
+
+          </Box>
+          {/*Main Body*/}
+      </Box>
   )
 }
 
--- mern_socialapp/client/src/scenes/widgets/UserWidget.jsx
+++ mern_socialapp/client/src/scenes/widgets/UserWidget.jsx
@@ -217,3 +217,5 @@ const UserWidget = ({userId, picturePath}) => {
         </WidgetWrapper>
     )
 }
+
+export default UserWidget
